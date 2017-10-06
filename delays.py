decd7313fe9d3e321ede17e792cd2ae9ac865387
Robert Maikher <robertem342@gmail.com>
2017-10-06 14:20:52 +0200

    Added legend to plot for 1_4 (and a version without lines)

--- delays.py
+++ delays.py
@@ -10,7 +10,9 @@ stdev = [0.276, 12.908, 20.117, 0.138, 0.275, 6.466]
 plt.ylabel('Delay (s)')
 plt.xlabel('Smart meters')
 
-plt.plot([1000,2000,4000], mu[0:3], 'ro', label='Number of servers: 2')
-plt.plot([1000,2000,4000], mu[3:6], 'bo', label='Number of servers: 4')
+plt.plot([1000,2000,4000], mu[0:3], 'ro-', label='Number of servers: 2')
+plt.plot([1000,2000,4000], mu[3:6], 'bo-', label='Number of servers: 4')
+
+plt.legend()
 
 plt.show()
